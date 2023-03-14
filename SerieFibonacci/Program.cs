cee343778e90939a1fd59bd7370f1dbb63f9121a
Cesar <cesaralex2018@gmail.com>
2023-03-14 12:46:45 -0600

    Agregar archivos de proyecto.

---
+++ SerieFibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int ValorInicial;
+int Cantidad;
+
+Console.WriteLine("Ingrese el numero inicial ");
+ValorInicial = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad de veces que se sumara ");
+Cantidad = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= Cantidad ; i++)
+{
+    ValorInicial = ValorInicial + i;
+    Console.WriteLine( "La serie de Fibonaaci es  " + ValorInicial);
+}
+
